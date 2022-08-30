3dc2c8a9cc2e2dbc6a249a0f6a560a468daabe76
nartnattakrivichian <nartnatta_kri@yahoo.com>
2022-08-31 01:35:59 +0700

    using pylint

--- week-1/silly_name_generator.py
+++ week-1/silly_name_generator.py
@@ -3,6 +3,7 @@ that conforms to established style guidelines."""
 import random
 import sys
 
+
 # Steps
 #     : Load a list of first names
 #     : Load a list of surnames
@@ -13,24 +14,30 @@ import sys
 #     : Print the name in order and in red front
 #     : Ask the user to quit or play again
 
-first = ["Big Burps", "Bill 'Beenie-Weenie'", "Bob 'Stinkbut'", "Boxelder",
-         "Chad", "Bowel Noises", "Dennis Clawhammer", "Cleet", "Winston 'Jazz Hands"]
+def main():
+    """Choose names at random from 2 lists of name and print to screen"""
+    first = ["Big Burps", "Bill 'Beenie-Weenie'", "Bob 'Stinkbut'", "Boxelder",
+             "Chad", "Bowel Noises", "Dennis Clawhammer", "Cleet", "Winston 'Jazz Hands"]
 
-last = ["Appleyard", "Bigmeat", "Bloominshine", "Goodpasture",
-        "Henderson", "Jingley-Schmidt", "Kingfish", "Wigglesworth", "Weewax"]
+    last = ["Appleyard", "Bigmeat", "Bloominshine", "Goodpasture",
+            "Henderson", "Jingley-Schmidt", "Kingfish", "Wigglesworth", "Weewax"]
 
-print("Welcome to the Psych 'Sidekick Name Picker'.\n")
+    print("Welcome to the Psych 'Sidekick Name Picker'.\n")
 
-while True:
-    firstName = random.choice(first)
-    lastName = random.choice(last)
+    while True:
+        first_name = random.choice(first)
+        last_name = random.choice(last)
 
-    print()
-    print("{} {}".format(firstName, lastName), file=sys.stderr)  # convert name variables to string
-    print()
+        print()
+        print("{} {}".format(first_name, last_name), file=sys.stderr)
+        print()
 
-    ask = input("Do you want to try again? (Press Enter else n to quit)\n")
-    if ask.lower() == 'n':
-        break
+        ask = input("Do you want to try again? (Press Enter else n to quit)\n")
+        if ask.lower() == 'n':
+            break
 
-input("\nPress Enter to exit")
+    input("\nPress Enter to exit")
+
+
+if __name__ == "__main__":
+    main()
